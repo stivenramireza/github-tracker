f97d5f6d9cae85c5e8963c50466ddc8ee244e3f8
Stiven Ramírez Arango <stivenramireza@gmail.com>
2024-03-21 22:09:31 -0500

    [ADD] Build artifacts

--- scripts/build_lambda_layers.sh
+++ scripts/build_lambda_layers.sh
@@ -10,5 +10,3 @@ BUCKET_NAME=github-tracker-lambda-stivenramireza
 DEPENDENCIES_LAMBDA_LAYER_PACKAGE=github-tracker-dependencies.zip
 zip -r ./dist/$DEPENDENCIES_LAMBDA_LAYER_PACKAGE venv
 aws s3 cp ./dist/$DEPENDENCIES_LAMBDA_LAYER_PACKAGE s3://$BUCKET_NAME/$DEPENDENCIES_LAMBDA_LAYER_PACKAGE
-
-deactivate
